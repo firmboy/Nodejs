84fbc8a8e072e272808de45dfc10fd170a394c8b
firmboy <yangjian5268@163.com>
2017-04-16 19:22:38 +0800

    change

--- Buffer/compare.js
+++ Buffer/compare.js
@@ -11,3 +11,17 @@ if(buf<0){
 }else {
 	console.log(buf1.toString()+"在"+buf2.toString()+"之后");
 }
+
+var buf1 = new Buffer("ABC");
+
+var buf2 = new Buffer("ABCD");
+
+var buf = buf1.compare(buf2);
+
+if(buf<0){
+		console.log(buf1.toString()+"在"+buf2.toString()+"之前");
+}else if (buf==0) {
+	console.log(buf1.toString()+"和"+buf2.toString()+"相同");
+}else{
+	console.log(buf1.toString()+"在"+buf2.toString()+"之后");
+}
